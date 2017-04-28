fd5f9296a13a2d6ec4681c0a2c76dac52cc99b58
jcy <jichengyue@14cells.com>
2017-04-28 18:33:15 +0800

    初始化红军与蓝军游戏网站前端

---
+++ src/components/TimeEntries.vue
@@ -0,0 +1,15 @@
+<template >
+  <div>
+    <router-link v-if="$route.path !== '/time-entries/log-time'"
+    to="/time-entries/log-time"
+    class="btn btn-primary">创建
+    </router-link>
+
+    <div v-if="$route.path === '/time-entries/log-time'">
+      <h3>创建</h3>
+    </div>
+
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
